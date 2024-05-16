6e77161eabe7fabbef5ed604f33074cd4ea3d67c
ked-k <kedkayz@gmail.com>
2024-05-16 11:56:29 +0300

    Update CapitalTransactionsController.php

--- app/Http/Controllers/CapitalTransactionsController.php
+++ app/Http/Controllers/CapitalTransactionsController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\capitalTransactions;
 use App\Models\Account;
+use App\Models\capitalTransactions;
 use App\Models\Currency;
 use App\Models\Forex\RateLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Throwable;
+
 class CapitalTransactionsController extends Controller
 {
     /**
@@ -18,10 +20,10 @@ class CapitalTransactionsController extends Controller
     public function index()
     {
         $values = capitalTransactions::leftJoin('users', 'capital_transactions.user_id', '=', 'users.id')
-        ->leftJoin('currencies','capital_transactions.currency_id','=','currencies.id')
-        ->leftJoin('accounts','capital_transactions.account','=','accounts.id')
-        ->orderBy('capital_transactions.id', 'desc')
-        ->select('*','capital_transactions.id as CTId')->get();
+            ->leftJoin('currencies', 'capital_transactions.currency_id', '=', 'currencies.id')
+            ->leftJoin('accounts', 'capital_transactions.account', '=', 'accounts.id')
+            ->orderBy('capital_transactions.id', 'desc')
+            ->select('*', 'capital_transactions.id as CTId')->get();
         return view('forex.capitaltransactions', compact('values'));
     }
 
@@ -33,20 +35,20 @@ class CapitalTransactionsController extends Controller
     public function create()
     {
 
-        $currencies = Currency::orderBy('currency_name', 'asc')->where('is_active','1')->get();
+        $currencies = Currency::orderBy('currency_name', 'asc')->where('is_active', '1')->get();
         //$code = $request->route('id'); //getting the request code
-        $accounts = Account::leftJoin('currencies','accounts.default_currency','=','currencies.id')->orderBy('accounts.id', 'desc')
-        ->select('*','accounts.id as actId')->where('accounts.is_active','1')->get();
-        return view('forex.newDeposit', compact('accounts','currencies'));
+        $accounts = Account::leftJoin('currencies', 'accounts.default_currency', '=', 'currencies.id')->orderBy('accounts.id', 'desc')
+            ->select('*', 'accounts.id as actId')->where('accounts.is_active', '1')->get();
+        return view('forex.newDeposit', compact('accounts', 'currencies'));
     }
     public function create2()
     {
 
-        $currencies = Currency::orderBy('currency_name', 'asc')->where('is_active','1')->get();
+        $currencies = Currency::orderBy('currency_name', 'asc')->where('is_active', '1')->get();
         //$code = $request->route('id'); //getting the request code
-        $accounts = Account::leftJoin('currencies','accounts.default_currency','=','currencies.id')->orderBy('accounts.id', 'desc')
-        ->select('*','accounts.id as actId')->where('accounts.is_active','1')->get();
-        return view('forex.newWithdraw', compact('accounts','currencies'));
+        $accounts = Account::leftJoin('currencies', 'accounts.default_currency', '=', 'currencies.id')->orderBy('accounts.id', 'desc')
+            ->select('*', 'accounts.id as actId')->where('accounts.is_active', '1')->get();
+        return view('forex.newWithdraw', compact('accounts', 'currencies'));
     }
 
     /**
@@ -57,57 +59,62 @@ class CapitalTransactionsController extends Controller
      */
     public function store(Request $request)
     {
-        DB::transaction(function () use ($request) {
-        $request->validate([
-            'currency'=> 'required',
-            'account'=> 'required',
-            'total_amount'=> 'required|numeric',
-            'total_foreign'=> 'required',
-            'Description'=> 'required',
-            'customer_id',
-            'charges'=> 'required',
-            'rate'=> 'required',
-            'date_added'=> 'required'
-            ]);
+        try {
+            DB::transaction(function () use ($request) {
+                $request->validate([
+                    'currency' => 'required',
+                    'account' => 'required',
+                    'total_amount' => 'required|numeric',
+                    'total_foreign' => 'required',
+                    'Description' => 'required',
+                    'customer_id',
+                    'charges' => 'required',
+                    'rate' => 'required',
+                    'date_added' => 'required',
+                ]);
 
-            $reff = 'DC'.mt_rand(1000, 9999).time();
-            $value = new capitalTransactions();
-            $value->reff_number = $reff;
-            $value->account = $request->input('account');
-            $value->currency_id = $request->input('currency');
-            $value->total_amount= $request->input('total_amount');
-            $value->foreign_amount= $request->input('initial_amount');
-            $value->rate= $request->input('rate');
-            $value->Description = $request->input('Description');
-            $value->charges= $request->input('charges');
-            $value->trans_type = 'deposit';
-            $value->trans_year = date("Y");
-            $value->trans_month = date("M-Y");
-            $value->trans_week = date("Y-M-W");
-            $value->date_added = $request->input('date_added');
-            $value->user_id =  auth()->user()->id;
-            $value->save();
-            $diffAmt = $request->input('total_amount');
-            $total_foreign = $request->input('total_foreign');
-            $this->updateRates($request->input('rate'), $request->input('currency'));
-            Account::where('id', $request->input('account'))->update(['available_balance' => DB::raw('available_balance + '.$diffAmt), 'foreign_amount' => DB::raw('foreign_amount + '.$total_foreign)]);            
+                $reff = 'DC' . mt_rand(1000, 9999) . time();
+                $value = new capitalTransactions();
+                $value->reff_number = $reff;
+                $value->account = $request->input('account');
+                $value->currency_id = $request->input('currency');
+                $value->total_amount = $request->input('total_amount');
+                $value->foreign_amount = $request->input('initial_amount');
+                $value->rate = $request->input('rate');
+                $value->Description = $request->input('Description');
+                $value->charges = $request->input('charges');
+                $value->trans_type = 'deposit';
+                $value->trans_year = date("Y");
+                $value->trans_month = date("M-Y");
+                $value->trans_week = date("Y-M-W");
+                $value->date_added = $request->input('date_added');
+                $value->user_id = auth()->user()->id;
+                $value->save();
+                $diffAmt = round($request->input('total_amount'),2);
+                $total_foreign = round($request->input('total_foreign'),2);
+                $this->updateRates($request->input('rate'), $request->input('currency'));
+                Account::where('id', $request->input('account'))->update(['available_balance' => DB::raw('available_balance + ' . $diffAmt), 'foreign_amount' => DB::raw('foreign_amount + ' . $total_foreign)]);
 
-        });
-        return  redirect('forex/capital')->with('success', 'Deposit Successfully added !!');
+            });
+
+            return redirect('forex/capital')->with('success', 'Deposit Successfully added !!');
+        } catch (Throwable $error) {
+            return redirect('forex/capital')->with('error', 'Deposit transaction !!');
+        }
     }
     public function updateRates($newBuyingRate, $currency_id)
     {
         // Get the current buying rate and profit percentage
-        $currency = Currency::where('id',$currency_id)->first();
+        $currency = Currency::where('id', $currency_id)->first();
         $currentBuyingRate = $currency->buying_rate;
         $currentSellingRate = $currency->selling_rate;
         $profitPercentage = $currency->percentage_rate;
 
         // Calculate the new average buying rate
-        
-        if($currentBuyingRate>0.1){
-        $newAverageBuyingRate = ($currentBuyingRate + $newBuyingRate) / 2;
-        }else{
+
+        if ($currentBuyingRate > 0.1) {
+            $newAverageBuyingRate = ($currentBuyingRate + $newBuyingRate) / 2;
+        } else {
             $newAverageBuyingRate = $newBuyingRate;
         }
         // dd($newAverageBuyingRate);
@@ -130,36 +137,36 @@ class CapitalTransactionsController extends Controller
     public function store2(Request $request)
     {
         $request->validate([
-            'currency'=> 'required',
-            'account'=> 'required',
-            'total_amount'=> 'required|numeric',
-            'total_foreign'=> 'required',
-            'Description'=> 'required',
+            'currency' => 'required',
+            'account' => 'required',
+            'total_amount' => 'required|numeric',
+            'total_foreign' => 'required',
+            'Description' => 'required',
             'customer_id',
-            'charges'=> 'required',
-            'date_added'=> 'required'
-            ]);
+            'charges' => 'required',
+            'date_added' => 'required',
+        ]);
 
-            $reff = 'WC'.mt_rand(1000, 9999).time();
-            $value = new capitalTransactions();
-            $value->reff_number = $reff;
-            $value->account = $request->input('account');
-            $value->currency_id = $request->input('currency');
-            $value->total_amount= $request->input('total_amount');
-            $value->foreign_amount= $request->input('total_foreign');
-            $value->Description = $request->input('Description');
-            $value->charges= $request->input('charges');
-            $value->trans_type = 'withdraw';
-            $value->trans_year = date("Y");
-            $value->trans_month = date("M-Y");
-            $value->trans_week = date("Y-M-W");
-            $value->date_added = $request->input('date_added');
-            $value->user_id =  auth()->user()->id;
-            $value->save();
-            $diffAmt = $request->input('total_amount');
-            Account::where('id', $request->input('account'))->update(['available_balance' => DB::raw('available_balance - '.$diffAmt)]);
+        $reff = 'WC' . mt_rand(1000, 9999) . time();
+        $value = new capitalTransactions();
+        $value->reff_number = $reff;
+        $value->account = $request->input('account');
+        $value->currency_id = $request->input('currency');
+        $value->total_amount = $request->input('total_amount');
+        $value->foreign_amount = $request->input('total_foreign');
+        $value->Description = $request->input('Description');
+        $value->charges = $request->input('charges');
+        $value->trans_type = 'withdraw';
+        $value->trans_year = date("Y");
+        $value->trans_month = date("M-Y");
+        $value->trans_week = date("Y-M-W");
+        $value->date_added = $request->input('date_added');
+        $value->user_id = auth()->user()->id;
+        $value->save();
+        $diffAmt = $request->input('total_amount');
+        Account::where('id', $request->input('account'))->update(['available_balance' => DB::raw('available_balance - ' . $diffAmt)]);
 
-            return  redirect('forex/capital')->with('success', 'Withdraw Successfully added !!');
+        return redirect('forex/capital')->with('success', 'Withdraw Successfully added !!');
     }
 
     /**
